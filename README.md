ccc76a47d7c4fbed9e63bbc2e709707e5dd0ecee
mofares22 <124840159+mofares22@users.noreply.github.com>
2023-02-09 02:00:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-h11c4d
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-h11c4d)
---
+++ index.ts
@@ -0,0 +1,20 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let momo1: Number;
+let momo2: number;
+momo1 = 153;
+momo2 = 457;
+console.log(momo2 + momo1);
+let mo1: number;
+let mo2: number;
+let mo3: number;
+mo1 = 150;
+mo2 = 250;
+mo3 = 350;
+console.log(mo1);
+console.log(mo2);
+console.log(mo3);
+console.log(mo1 + mo2 + mo3);
+
